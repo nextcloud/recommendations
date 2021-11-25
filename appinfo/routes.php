f35c50eb7aa70f1dc8149d6de289ad7cbe3e1d60
sualko <klaus@jsxc.org>
2021-11-25 14:42:31 +0100

    add restful API
    fix #457
    
    Signed-off-by: sualko <klaus@jsxc.org>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -30,5 +30,7 @@ return [
 		['name' => 'settings#setSetting', 'url' => '/settings/{key}', 'verb' => 'PUT'],
 		['name' => 'recommendation#index', 'url' => '/api/recommendations', 'verb' => 'GET'],
 		['name' => 'recommendation#always', 'url' => '/api/recommendations/always', 'verb' => 'GET'],
+		['name' => 'recommendation_api#index', 'url' => '/api/v1/recommendations', 'verb' => 'GET'],
+		['name' => 'recommendation_api#always', 'url' => '/api/v1/recommendations/always', 'verb' => 'GET'],
 	],
 ];
