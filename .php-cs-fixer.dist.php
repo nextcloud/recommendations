33131363b7516302a85f2bf01a5c722f2ad4301d
Carl Schwan <carl@carlschwan.eu>
2022-05-05 16:42:03 +0200

    Update github jobs
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,5 +9,11 @@ use Nextcloud\CodingStandard\Config;
 $config = new Config();
 $config
 	->getFinder()
-	->in(__DIR__ . '/lib');
+	->notPath('build')
+	->notPath('composer')
+	->notPath('l10n')
+	->notPath('node_modules')
+	->notPath('src')
+	->notPath('vendor')
+	->in(__DIR__);
 return $config;
