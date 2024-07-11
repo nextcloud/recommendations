06a9b481c588c81d44c72d1b7092ea0b6f44b6ae
Andy Scherzinger <info@andy-scherzinger.de>
2024-07-11 18:06:38 +0200

    ci: Add .license file creation
    Co-authored-by: Ferdinand Thiessen <opensource@fthiessen.de>
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,32 @@
  */
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const TerserPlugin = require('terser-webpack-plugin')
+const WebpackSPDXPlugin = require('./build-js/WebpackSPDXPlugin.js')
+const webpackRules = require('@nextcloud/webpack-vue-config/rules')
 
 webpackConfig.entry.dashboard = path.resolve(path.join('src', 'dashboard.js'))
+
+webpackConfig.optimization.minimizer = [new TerserPlugin({
+	extractComments: false,
+	terserOptions: {
+		format: {
+			comments: false,
+		},
+	},
+})]
+
+webpackConfig.plugins = [
+	...webpackConfig.plugins,
+	// Generate reuse license files
+	new WebpackSPDXPlugin({
+		override: {
+			// TODO: Remove if they fixed the license in the package.json
+			'@nextcloud/axios': 'GPL-3.0-or-later',
+			'@nextcloud/vue': 'AGPL-3.0-or-later',
+			'nextcloud-vue-collections': 'AGPL-3.0-or-later',
+		}
+	}),
+]
 
 module.exports = webpackConfig
