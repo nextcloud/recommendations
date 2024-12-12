52d0440756dac7d14c057dfa3ee5ac7831102372
tobiasKaminsky <tobias@kaminsky.me>
2024-12-12 08:44:14 +0100

    switch index to ocs

--- src/service/RecommendationService.js
+++ src/service/RecommendationService.js
@@ -6,8 +6,9 @@ import Axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
 
 export const fetchRecommendedFiles = (always) => {
-	const url = generateUrl('/apps/recommendations/api/recommendations' + (always ? '/always' : ''))
+	const url = generateUrl('/ocs/v2.php/apps/recommendations/api/v1/recommendations' + (always ? '/always' : ''),
+		{}, { noRewrite: false })
 
 	return Axios.get(url)
-		.then(resp => resp.data)
+		.then(resp => resp.data.ocs.data)
 }
