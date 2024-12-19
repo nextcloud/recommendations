2b3db5ba4a89c72b92c4245cdb037762dd4e7617
provokateurin <kate@provokateurin.de>
2024-12-19 08:58:36 +0100

    refactor(RecommendationController): Migrate to OCS
    Signed-off-by: provokateurin <kate@provokateurin.de>
    Signed-off-by: Christopher Ng <chrng8@gmail.com>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -12,7 +12,9 @@ return [
 	'routes' => [
 		['name' => 'settings#getSettings', 'url' => '/settings', 'verb' => 'GET'],
 		['name' => 'settings#setSetting', 'url' => '/settings/{key}', 'verb' => 'PUT'],
-		['name' => 'recommendation#index', 'url' => '/api/recommendations', 'verb' => 'GET'],
-		['name' => 'recommendation#always', 'url' => '/api/recommendations/always', 'verb' => 'GET'],
+	],
+	'ocs' => [
+		['name' => 'recommendation#always', 'url' => '/api/v1/recommendations/always', 'verb' => 'GET'],
+		['name' => 'recommendation#index', 'url' => '/api/v1/recommendations', 'verb' => 'GET'],
 	],
 ];
---
+++ lib/ResponseDefinitions.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/**
+ * @psalm-type RecommendationsRecommendedFile = array{
+ *     id: string,
+ *     timestamp: int,
+ *     name: string,
+ *     directory: string,
+ *     extension: string,
+ *     mimeType: string,
+ *     hasPreview: bool,
+ *     reason: string,
+ * }
+ *
+ * @psalm-suppress UnusedClass
+ */
+class ResponseDefinitions {
+}
