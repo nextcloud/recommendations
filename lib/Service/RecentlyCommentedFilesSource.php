3e2f5fecd87e95c147ad589c37440428de6fcece
Josh <josh.t.richards@gmail.com>
2026-04-21 15:55:42 -0400

    perf(recommendations): avoid scanning all commented files
    Stop scanning comments once enough recent unique files have been found.
    
    Signed-off-by: Josh <josh.t.richards@gmail.com>

--- lib/Service/RecentlyCommentedFilesSource.php
+++ lib/Service/RecentlyCommentedFilesSource.php
@@ -2,45 +2,20 @@
 
 /**
  * SPDX-FileCopyrightText: 2018 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 declare(strict_types=1);
 
-/**
- * @copyright 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
- *
- * @author 2019 Christoph Wurst <christoph@winzerhof-wurst.at>
- *
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 namespace OCA\Recommendations\Service;
 
-use Generator;
 use OCP\Comments\IComment;
 use OCP\Comments\ICommentsManager;
 use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
 use OCP\IL10N;
 use OCP\IUser;
-use function array_map;
-use function array_slice;
-use function iterator_to_array;
-use function usort;
 
 class RecentlyCommentedFilesSource implements IRecommendationSource {
 
@@ -49,11 +24,15 @@ class RecentlyCommentedFilesSource implements IRecommendationSource {
 	public function __construct(
 		private readonly ICommentsManager $commentsManager,
 		private readonly IRootFolder $rootFolder,
-		private readonly IL10N $l10n,
 	) {
 	}
 
+	/**
+	 * @return IComment[]
+	 */
 	private function getCommentsPage(int $offset, int $pageSize): array {
+		// Comment search returns newest comments first, so the first resolvable
+		// unique file IDs we encounter are the most recent recommendations.
 		return $this->commentsManager->search(
 			'',
 			'files',
@@ -78,63 +57,58 @@ class RecentlyCommentedFilesSource implements IRecommendationSource {
 	}
 
 	/**
-	 * @param IUser $user
-	 *
-	 * @return Generator<FileWithComments>
-	 */
-	private function getAllCommentedFiles(IUser $user): Generator {
-		$offset = 0;
-		$pageSize = 100;
-
-		$userFolder = $this->rootFolder->getUserFolder($user->getUID());
-
-		while (count($page = $this->getCommentsPage($offset, $pageSize)) > 0) {
-			foreach ($page as $comment) {
-				$commentedFile = $this->getCommentedFile($comment, $userFolder);
-				if (!is_null($commentedFile)) {
-					yield $commentedFile;
-				}
-			}
-
-			$offset += $pageSize;
-		}
-	}
-
-	/**
-	 * @param FileWithComments[] $original
-	 * @return FileWithComments[]
-	 */
-	private function sortCommentedFiles(array $original): array {
-		usort($original, function (FileWithComments $a, FileWithComments $b) {
-			return $b->getComment()->getCreationDateTime()->getTimestamp() - $a->getComment()->getCreationDateTime()->getTimestamp();
-		});
-		return $original;
-	}
-
-	/**
-	 * @param FileWithComments[] $comments
-	 * @return FileWithComments[]
-	 */
-	private function getNMostRecentlyCommenedFiles(array $comments, int $n): array {
-		$sorted = $this->sortCommentedFiles($comments);
-
-		return array_slice($sorted, 0, $n);
-	}
-
-	/**
 	 * @return IRecommendation[]
 	 */
 	#[\Override]
 	public function getMostRecentRecommendation(IUser $user, int $max): array {
-		$all = iterator_to_array($this->getAllCommentedFiles($user));
+		if ($max <= 0) {
+			return [];
+		}
 
-		return array_map(function (FileWithComments $file) {
-			return new RecommendedFile(
-				$file->getDirectory(),
-				$file->getNode(),
-				$file->getComment()->getCreationDateTime()->getTimestamp(),
-				self::REASON,
-			);
-		}, $this->getNMostRecentlyCommenedFiles($all, $max));
+		$offset = 0;
+		$pageSize = 100;
+		$results = [];
+		$seenFileIds = [];
+
+		$userFolder = $this->rootFolder->getUserFolder($user->getUID());
+
+		while (count($results) < $max) {
+			$page = $this->getCommentsPage($offset, $pageSize);
+			if ($page === []) {
+				break;
+			}
+
+			foreach ($page as $comment) {
+				$fileId = (int)$comment->getObjectId();
+				if (isset($seenFileIds[$fileId])) {
+					continue;
+				}
+
+				$commentedFile = $this->getCommentedFile($comment, $userFolder);
+				if ($commentedFile === null) {
+					continue;
+				}
+
+				$seenFileIds[$fileId] = true;
+				$results[] = new RecommendedFile(
+					$commentedFile->getDirectory(),
+					$commentedFile->getNode(),
+					$comment->getCreationDateTime()->getTimestamp(),
+					self::REASON,
+				);
+
+				if (count($results) >= $max) {
+					break 2;
+				}
+			}
+
+			if (count($page) < $pageSize) {
+				break;
+			}
+
+			$offset += $pageSize;
+		}
+
+		return $results;
 	}
 }
